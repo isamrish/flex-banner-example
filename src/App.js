f9aa8eca4e9c56f453db7ebbaf4d34d78bd23a75
isamrish <askmaurya48@gmail.com>
2020-01-26 23:20:01 +0530

    Update
    Signed-off-by: isamrish <askmaurya48@gmail.com>

--- src/App.js
+++ src/App.js
@@ -4,27 +4,19 @@ import "./App.css";
 import FlexBanner from "flex-banner";
 function App() {
   return (
-    <div className="App">
+    <div>
       <FlexBanner
         title="Fully responsive react banner for websites"
         ctaLink="https://github.com/IsAmrish/flex-banner"
         ctaTitle="Learn More"
         isCenter={true}
+        delayToShowBanner={2}
       />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        style={{ height: "500px", padding: "30px", backgroundColor: "#54AB6A" }}
+      >
+        <h2 style={{ margin: "0px" }}>Welcome to FlexBanner PlayGround</h2>
+      </div>
     </div>
   );
 }
